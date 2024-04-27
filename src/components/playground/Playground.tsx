46a1376d9aa349ee86aa1417cc4538786ed7afb6
Kyungmin Kim <kyungmin.kim.31@gmail.com>
2024-04-27 14:17:59 +0900

    add scheduler

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -13,9 +13,12 @@ const S = {
 
 // https://github.com/mdn/webaudio-examples/blob/main/step-sequencer/index.html#L426
 function Playground () {
+
   const audioCtx = new AudioContext();
   const [clickSample, setClickSample] = useState<AudioBuffer>();
+
   useEffect(() => {
+    console.log("rendered");
     const setupSample = async (audioCtx: AudioContext) => {
       return await getFile(audioCtx, clickUrl);
     };
@@ -32,9 +35,27 @@ function Playground () {
 
   }, []);
 
-  const playSample = (se: SyntheticEvent) => {
-    se.preventDefault();
 
+  const tempo = 60.0;
+  const lookahead = 25.0;
+  const schedulerAheadTime = 0.1
+
+  let currentNote = 0;
+  let nextNoteTime = 0;
+  const nextNote = () => {
+    const secondsPerBeat = 60.0 / tempo;
+
+    nextNoteTime += secondsPerBeat; // Add beat length to last beat time
+
+    // Advance the beat number, wrap to zero when reaching 4
+    currentNote = (currentNote + 1) % 4;
+  };
+
+  const scheduleNote = (time: number) => {
+    playSample(time);
+  };
+
+  const playSample = (time: number) => {
     const playbackRate = 1;
     const sampleSource = new AudioBufferSourceNode(audioCtx, {
       buffer: clickSample,
@@ -43,12 +64,34 @@ function Playground () {
     sampleSource.connect(audioCtx.destination);
     sampleSource.start(0);
     return sampleSource;
+  }
+
+  let timerID;
+  const scheduler = () => {
+    while(nextNoteTime < audioCtx.currentTime + schedulerAheadTime) {
+      scheduleNote(nextNoteTime);
+      nextNote();
+    }
+    timerID = setTimeout(scheduler, lookahead);
+  }
+
+  const play = (se: SyntheticEvent) => {
+    se.preventDefault();
+
+    if(audioCtx.state === "suspended") {
+      audioCtx.resume().then(() => console.log('audio ctx resumed'));
+    }
+
+    nextNoteTime = audioCtx.currentTime;
+    scheduler();
   };
 
   return (
     <S.Container>
-      {clickSample ? "a sample is loaded" : "unloaded" }
-      <button onClick={playSample}>play</button>
+      <div>
+        {clickSample ? "a sample is loaded" : "unloaded" }
+      </div>
+      <button onClick={play}>play</button>
     </S.Container>
 );
 }
